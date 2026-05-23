bf303245f649d6dd0a03a90a0688185331419454
Rui Ogawa <ruiogawa@gmail.com>
2026-05-23 15:43:31 -0300

    Change app.run to use host '0.0.0.0'

--- Bdmep_app.py
+++ Bdmep_app.py
@@ -1038,4 +1038,4 @@ if __name__ == "__main__":
     print(" BDMEP Downloader")
     print(" Abra no navegador: http://localhost:5000")
     print("=" * 50)
-    app.run(debug=False, port=5000)
+    app.run(debug=False, host="0.0.0.0", port=5000)
